5c4cfd0b394508ee12f3ac2d1c9915107a1f94b2
Heitor <heitoraugustobilibio4@gmail.com>
2025-07-20 22:30:23 -0300

    Testes RabbitMq

---
+++ EnvioRabbit/EnvioRabbit/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+class Program
+{
+    public static async Task Main()
+    {
+        var factory = new ConnectionFactory { HostName = "localhost" };
+
+        using var connection = await factory.CreateConnectionAsync();
+
+        using var channel = await connection.CreateChannelAsync();
+
+        await channel.ExchangeDeclareAsync("Ex", ExchangeType.Direct);
+        await channel.QueueDeclareAsync(queue: "fila", durable: false, exclusive: false, autoDelete: false, arguments: null);
+        await channel.QueueBindAsync("fila", "Ex", "routingKey", null);
+
+        while (true)
+        {
+            Console.WriteLine("Escreva uma mensagem");
+            var message = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(message))
+            {
+                Console.WriteLine("Fechando a Aplicação");
+                break;
+            }
+
+            var body = Encoding.UTF8.GetBytes(message);
+            var props = new BasicProperties();
+
+            await channel.BasicPublishAsync(exchange: "Ex",
+                                 routingKey: "routingKey",
+                                 false,
+                                 basicProperties: props,
+                                 body: body);
+
+            Console.WriteLine($"Enviando a mensagem {message}");
+        }
+       
+
+
+    }
+}
---
+++ RecebeRabbit/RecebeRabbit/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RecebeRabbit
+{
+    public class Program
+    {
+        public static async Task Main()
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+
+            await channel.QueueDeclareAsync(queue: "fila", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            Console.WriteLine("Aguardando mensagens na fila...");  
+
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            consumer.ReceivedAsync += async (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                await Task.Delay(1000);
+                var message = System.Text.Encoding.UTF8.GetString(body);
+                Console.WriteLine($"Mensagem recebida: {message}");
+                await channel.BasicAckAsync(deliveryTag: ea.DeliveryTag, multiple: false);
+            };
+
+            await channel.BasicConsumeAsync("fila", false, consumer);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
